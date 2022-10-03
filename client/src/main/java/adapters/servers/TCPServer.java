085644f9cf752f2862011ba7b13fc0e847f9b461
Aleksey Panas <panasaleksey3@gmail.com>
2022-10-02 21:47:23 -0400

    Partially implement TCP connect

--- client/src/main/java/adapters/servers/TCPServer.java
+++ client/src/main/java/adapters/servers/TCPServer.java
@@ -3,22 +3,40 @@ package adapters.servers;
 import adapters.ViewModel;
 import adapters.servers.ds.ConnectReturnData;
 import java.net.*;
+import java.io.*;
 
 // Layer: DRIVERS & FRAMEWORKS
 
 public class TCPServer extends ServerAdapter {
     // TODO: Change this to pull from a configuration file.
-    private final String SERVER_IP = "localhost";
-    private final String SERVER_PORT = "42069";
+    private static final String SERVER_IP = "localhost";
+    private static final int SERVER_PORT = 42069;
+    private static final int CONNECTION_TIMEOUT = 1000;
+
+    private final Socket serverConnection;
 
     public TCPServer (ViewModel viewM) {
         super(viewM);
+
+        // Create blank socket instance.
+        serverConnection = new Socket();
     }
 
     @Override
     protected ConnectReturnData connect() {
-        // TODO: Implement this method
-        return new ConnectReturnData(false, "Under construction",
-                ConnectReturnData.CLIENT_ID_WHEN_FAIL);
+        try {
+            InetSocketAddress addr = new InetSocketAddress(SERVER_IP, SERVER_PORT);
+            serverConnection.connect(addr, CONNECTION_TIMEOUT);
+        }
+
+        // Connection did not go through
+        catch (IOException e) {
+            return new ConnectReturnData(false, "Connection failed",
+                    ConnectReturnData.CLIENT_ID_WHEN_FAIL);
+        }
+
+        // TODO: Retrieve client ID from server and return it below (replace 0).
+
+        return new ConnectReturnData(true, "Connected successfully", 0);
     }
 }
